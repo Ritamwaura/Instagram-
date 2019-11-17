93c8bfc1c2818f15a3bec425d08e3819c71257dc
Your Name <mwaurarita2019@gmail.com>
2019-11-17 23:30:39 +0300

    add a decorator to define our login

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -13,3 +13,36 @@ from .models import Post, Profile
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserRegistrationForm()
+        params = {
+            'form': form
+        }
+    return render(request, 'registration/register.html', locals())
+
+
+@login_required(login_url='login')
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.all()
+    posts = Post.get_all_posts()
+    print(posts)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
