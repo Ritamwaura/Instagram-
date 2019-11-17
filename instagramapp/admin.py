634b05c2f1ea9987a81a265d5f490789c40a8fed
Your Name <mwaurarita2019@gmail.com>
2019-11-17 22:21:24 +0300

    add admin form

---
+++ instagramapp/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Profile, Post, Comment, Likes
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Likes)
+
