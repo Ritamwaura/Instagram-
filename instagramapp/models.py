d3fc9752919fe8687fc66f9cb58948c51a20f3d5
Your Name <mwaurarita2019@gmail.com>
2019-11-17 22:44:21 +0300

    add  a clas of meta db table

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -8,3 +8,17 @@ from django.dispatch import receiver
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    profile_pic = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="Users biography", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    
+    class Meta:
+        db_table = 'profile'
+
+    def __str__(self):
+        return f'{self.user.username} : {self.location}'    
+
