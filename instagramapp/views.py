e43958e35582646dae031a32b35c242a248a5fd6
Your Name <mwaurarita2019@gmail.com>
2019-11-17 23:28:56 +0300

    add our import our http responceand render

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm, UserRegistrationForm
+from .models import Post, Profile
+
 
 from django.shortcuts import render
 
